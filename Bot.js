ad5c4658278caf4ff40f9bdf13187320f36190cb
Heroos <44765221+Heroos@users.noreply.github.com>
2018-11-17 00:56:47 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,25 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-module.exports.run = (client, message, args) => {
 	var prefix = "$";
-  if (message.author.id !== ('175465689027444736')) return message.channel.send("Huh.");
-  const status = args.join(' ');
-  if (status.length === 0) {
-    const embed = new Discord.RichEmbed()
-      .setColor("#7289DA")
-      .setDescription(':negative_squared_cross_mark: Name streaming status!');
-    message.channel.send({ embed });
-  }
 
-  else if (status.length !== 0) {
-   client.user.setPresence({ game: { name: `${status}`, url: 'https://twitch.tv/', type: 1 } });
-  const embed = new Discord.RichEmbed()
-    .setColor("#7289DA")
-    .setDescription(':white_check_mark: You sucessfully changed streaming status');
-  message.channel.send({ embed });
-    });
-}
+	bot.user.setGame('لا اله الا الله محمد رسول الله');
+
+	bot.user.setStatus(dmd);
+
+	bot.user.setGame('لا اله الا الله محمد رسول الله, 'https://www.twitch.tv/');
+
+});
 
 
 client.login(process.env.BOT_TOKEN);
